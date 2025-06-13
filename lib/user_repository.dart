9de36d1f4efcb85fff3a8eee36996c7526c5b057
Valdez <bentirazv@gmail.com>
2025-06-13 20:52:35 +0700

    Update user_repository.dart

--- lib/user_repository.dart
+++ lib/user_repository.dart
@@ -1,24 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'user_model.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 class UserRepository {
   final String baseUrl = 'http://172.20.10.3:8080'; // Ganti dengan base URL backend kamu
 
   Future<void> addUser(User user, String uid) async {
-    final url = Uri.parse('$baseUrl/users');
-
-
-    final userMap = user.toMap();
-
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(userMap),
-    );
-
-    if (response.statusCode != 200) {
-      throw Exception('Gagal menyimpan user ke backend: ${response.body}');
-    }
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(uid)
+        .set(user.toMap());
   }
 }
